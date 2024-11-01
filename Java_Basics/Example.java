6983dc3db0fe166df89af19f8ab42c987ebeac06
Festongithub <fkemboik@gmail.com>
2024-11-01 13:58:19 +0300

    Notifications

---
+++ Java_Basics/Example.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is a simple Example class 
+ * with a System out printyout
+ **/
+class Example {
+    public static void main(String args[]){
+
+        int num = 100;
+        num = num * 2;
+
+        System.out.println("New number is: " + num);
+        System.out.println("The rise of Java");
+    }
+}
---
+++ Java_Basics/ifSample.java
@@ -0,0 +1,17 @@
+
+/**
+ * tes the if flow statement
+ * 
+ **/
+
+class IfSample{
+    public static void main(String args[]){
+        int x, y;
+        x = 10;
+        y = 20;
+
+        if(x < y) System.out.println("x is less than y");
+        if(x == y) System.out.println("x is equal to y");
+        if(y > x) System.out.println("y is more than x");
+    }
+}
